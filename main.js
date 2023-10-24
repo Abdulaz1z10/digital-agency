5de89dd5e0a35ca0235737add06b7136963cbbe6
Your Name <you@example.com>
2023-10-25 01:01:54 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+// let text = prompt('text kirit')
+// let arr = ['a', 'b', 'c']
+
+// function fn (){
+//     for(let i = 0; i < arr.length; i++){
+//         for(let g = 0; g < text.length; g++){
+//             if(arr[i] === text[g]){
+//                 console.log(i)
+//             }
+//         }
+//     }
+// }
+// fn()
+
+// const fn = (...rest) =>{
+//     let arr = [1, false, 4, true, 'false', undefined];
+//     for(let i = 0; i < arr.length; i++){
+//         for(let g = 0; g < rest.length; g++){
+//             if(typeof(arr[i]) == typeof(rest[g])){
+
+//                 console.log(arr[i])
+//             }
+//         }
+//     }
+// }
+// fn(1,false)
+
+// let arr = [parseInt(Math.random() * 100), 5, 7, 9,]
+
+// if(arr[0] % 2 == 0){
+//     arr.splice(0,1,arr[0] +1)
+// }else{
+//     arr.splice(0,1,arr[0] +1)
+// }
+// console.log(arr)
+
+
+let arr = [5,8,9,17,0,2]
+
+for(let i = 0; i < arr.length; i++){
+    if(arr[0] < arr[i]);
+} else if(arr[0] < arr[2]){
+    
+}
